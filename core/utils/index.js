3f671c3171a894d7e2c2af60e3e14d566c863fa7
wonder-girl92 <81470730+wonder-girl92@users.noreply.github.com>
2022-02-12 03:54:14 +0300

    Updates

--- core/utils/index.js
+++
@@ -1,8 +0,0 @@
-import moment from 'moment'
-
-export const calculateSumOfNumbers = (numbers) =>
-  numbers.reduce((acc, cur) => acc + cur, 0)
-
-export const getFormattedTime = (date) => {
-  return moment(date).format('MMMM Do YYYY, h:mm:ss a')
-}
